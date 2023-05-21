e66f4c95943d4520e0eeabb9f170a166ad8c08c1
younglee0280 <younglee0280@gmail.com>
2023-05-22 05:12:02 +0900

    2023/05/21 delete app.js, index2, input.jsx, styles.css

--- alarm/alarm.jsx
+++ alarm/alarm.jsx
@@ -4,4 +4,27 @@ import './alarm.css';
 const Alarm = (props) => {
     const [isEditing, setIsEditing] = useState(false);
     const [isCompleted, setIsCompleted] = useState(false);
+    const [inputText, setInputText] = useState(props.title);
+
+	const doubleClickHandler = () => {
+		console.log('dd');
+		setIsEditing(true);
+	};
+
+    let element;
+	if (isEditing) {
+		element = (
+			<>
+				<input className={`${isEditing && 'edit'}`}
+					type="text"
+					value={inputText}
+					onChange={updateHandler}
+					onKeyDown={updateHandler}
+					onBlur={blurHandler}
+					autoFocus />
+				<span>{props.ampm} {props.hour} 시 {props.minute} 분</span>
+			</>
+		);
+
+}
 }
